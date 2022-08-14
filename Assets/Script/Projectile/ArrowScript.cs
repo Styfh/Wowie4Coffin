a534b6ad221f3a332358fb082ae7053815ac5061
PapaDevilno <94031852+PapaDevilno@users.noreply.github.com>
2022-08-14 16:45:41 +0700

    Player Movement, Shooting, Damage

---
+++ Assets/Script/Projectile/ArrowScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    //public GameObject hitEffect;
+    public int damage = 40;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemiScript enemy = hitInfo.GetComponent<EnemiScript>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+    }
+}
